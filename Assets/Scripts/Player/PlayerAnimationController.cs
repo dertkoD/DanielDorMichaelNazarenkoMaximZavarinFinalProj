f759d80c1d749cd698c5f22dd983d4015f93772b
Dertko Daniil <64313607+dertkoD@users.noreply.github.com>
2026-03-20 22:51:21 +0200

    Edit animations

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -6,19 +6,19 @@ public class PlayerAnimationController : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private Transform visualRoot;
     [SerializeField] private PlayerMovement playerMovement;
-    [SerializeField] private PlayerJump playerJump;
     [SerializeField] private GroundChecker groundChecker;
     [SerializeField] private PlayerWeaponController playerWeaponController;
 
     [Header("Animator Params")]
     [SerializeField] private string xVelocityParam = "xVelocity";
     [SerializeField] private string yVelocityParam = "yVelocity";
-    [SerializeField] private string isGroundedParam = "IsGrounded";
+    [SerializeField] private string jumpParam = "Jump";
     [SerializeField] private string hasWeaponParam = "HasWeapon";
-    [SerializeField] private string verticalVelocityParam = "VerticalVelocity";
+    [SerializeField] private string deathParam = "Death";
+    [SerializeField] private string aimParam = "Aim";
 
-    [Header("Tuning")]
-    [SerializeField] private float velocityNormalization = 6f;
+    [Header("Speed Mapping")]
+    [SerializeField] private float maxAnimSpeed = 6f;
     [SerializeField] private float dampTime = 0.08f;
 
     private void Update()
@@ -29,22 +29,13 @@ public class PlayerAnimationController : MonoBehaviour
         Vector3 worldVelocity = playerMovement.HorizontalVelocity;
         Vector3 localVelocity = visualRoot.InverseTransformDirection(worldVelocity);
 
-        float x = Mathf.Clamp(localVelocity.x / velocityNormalization, -1f, 1f);
-        float y = Mathf.Clamp(localVelocity.z / velocityNormalization, -1f, 1f);
+        float x = Mathf.Clamp(localVelocity.x / maxAnimSpeed, -1f, 1f);
+        float y = Mathf.Clamp(localVelocity.z / maxAnimSpeed, -1f, 1f);
 
         animator.SetFloat(xVelocityParam, x, dampTime, Time.deltaTime);
         animator.SetFloat(yVelocityParam, y, dampTime, Time.deltaTime);
 
-        bool isGrounded = groundChecker != null && groundChecker.IsGrounded;
-        bool hasWeapon = playerWeaponController != null && playerWeaponController.HasWeapon;
-
-        animator.SetBool(isGroundedParam, isGrounded);
-        animator.SetBool(hasWeaponParam, hasWeapon);
-        animator.SetFloat(verticalVelocityParam, playerMovement.CurrentVelocity.y);
-    }
-
-    public void PlayJumpStart()
-    {
-        animator.SetTrigger("JumpTrigger");
+        animator.SetBool(hasWeaponParam, playerWeaponController != null && playerWeaponController.HasWeapon);
+        animator.SetBool(aimParam, true);
     }
 }
