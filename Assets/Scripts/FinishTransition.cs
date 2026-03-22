8e68ff247321c24d48513338abe6322dd00924ed
Mike-Avrius <mike.nidam@gmail.com>
2026-03-22 16:54:20 +0200

    TimerSave

--- Assets/Scripts/FinishTransition.cs
+++ Assets/Scripts/FinishTransition.cs
@@ -4,11 +4,15 @@ public class FinishTransition : MonoBehaviour
 {
     const string SHOOTER_SCENE_NAME = "ShootScene";
     [SerializeField] private ProgressBar progressBar;
+    [SerializeField] private Timer _timer;
+    
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             progressBar.AddProgress();
+            _timer.StopTimerOnFinish();
             SceneTransitionManager.Instance.TransitionToScene(SHOOTER_SCENE_NAME);
         }
     }
